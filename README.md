9ee928254ae91428ab34a821f76709c2e41bf8dc
Andy <andrewryzen@gmail.com>
2020-03-30 21:09:20 -0400

    Finished the app.js

---
+++ README.md
@@ -0,0 +1,15 @@
+# Team-Generator
+
+Command line app that generates an HTML with employee names, emails, id numbers ect.
+
+
+
+**Instructions:**
+
+    1. Run your terminal with the correct path
+    3. In the terminal, run the command: npm install
+    4. Run the command: node app.js
+    5. Go through the prompts and complete.
+
+
+![Example](TeamExample.png)
---
+++ app.js
@@ -0,0 +1,181 @@
+const inquirer = require("inquirer");
+const jest = require("jest");
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const generateHTML = require("./lib/generateHTML");
+const fs = require("fs");
+const employeeArr = [];
+const mapArr = require("./lib/mapArr");
+
+function init() {
+    function createManager() {
+
+        const questions = generateQuestions("Manager")
+
+        return inquirer.prompt(questions)
+            .then(function(answers) {
+                const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+                employeeArr.push(manager);
+                choose();
+            })
+    };
+
+    function choose() {
+        const chooseEmployee = [{
+            type: "input",
+            name: "continue",
+            message: "Do you want to add another employee [y/n]"
+        }]
+        return inquirer.prompt(chooseEmployee)
+            .then(function(answers) {
+                if (answers.continue.toLowerCase() === "y") {
+                    chooseYes();
+                }
+                if (answers.continue.toLowerCase() === "n") {
+                    mapArr(employeeArr);
+                }
+            })
+    };
+    
+    function chooseYes() {
+        const chooseYep = [{
+            type: "list",
+            name: "choose",
+            message: "Choose an employee",
+            choices: [
+                "Engineer",
+                "Intern"
+            ]
+        }]
+        return inquirer.prompt(chooseYep)
+            .then(function(answers) {
+            
+                if (answers.choose === "Engineer") {
+                    createEngineer();
+                }
+                
+                if (answers.choose === "Intern") {
+                    createIntern();
+                }
+            })
+    }
+
+    function generateQuestions(employeeType) {
+        const questions = [{
+                type: "input",
+                name: "name",
+                message: "Name?",
+                validate: (input) => {
+                    var letters = /^[A-Za-z]+$/;
+                    if (input.match(letters)) {
+                        return true;
+                    } else {
+                        return "Letters only!";
+                    }
+                }
+            },
+
+            {type: "input",
+                name: "id",
+                message: "ID number?",
+                validate: (input) => {
+                    var letters = /^[0-9]+$/;
+                    if (input.match(letters)) {
+                        return true;
+                    } else {
+                        return "Numbers only!";
+                    }
+                }
+            },
+
+            {type: "input",
+                name: "email",
+                message: "Enter email",
+                validate: (input) => {
+                    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                    if (input.match(re)) {
+                        return true;
+                    } else {
+                        return "Not a valid email address";
+                    }
+                }
+            },
+        ]
+    
+        const managerQuestion = {
+            type: "input",
+            name: "officeNumber",
+            message: "Office Number",
+            validate: (input) => {
+                var letters = /^[0-9]+$/;
+                if (input.match(letters)) {
+                    return true;
+                } else {
+                    return "Numbers only!";
+                }
+            }
+        }
+    
+        const EngineerQuestion = {
+            type: "input",
+            name: "github",
+            message: "Github username?",
+            validate: (input) => {
+                if (input.trim() !== "") {
+                    return true;
+                } else {
+                    return "Must enter a username";
+                }
+            }
+        }
+    
+        const InternQuestion = {
+            type: "input",
+            name: "school",
+            message: "What is the name of your school?",
+            validate: (input) => {
+                if (input.trim() !== "") {
+                    return true;
+                } else {
+                    return "Must enter a school name";
+                }
+            }
+        }
+        switch (employeeType) {
+            case "Engineer":
+                questions.push(EngineerQuestion);
+                break;
+            case "Intern":
+                questions.push(InternQuestion);
+                break;
+            case "Manager":
+                questions.push(managerQuestion);
+                break;
+        }
+        return questions;
+    }
+
+    function createEngineer() {
+        const questions = generateQuestions("Engineer")
+        return inquirer.prompt(questions)
+            .then(function(answers) {
+                choose()
+                const newEngineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+                employeeArr.push(newEngineer);
+            })
+    };
+    function createIntern() {
+        const questions = generateQuestions("Intern")
+
+        return inquirer.prompt(questions)
+            .then(function(answers) {
+                choose()
+                const newIntern = new Intern(answers.name, answers.id, answers.email, answers.school);
+                employeeArr.push(newIntern);
+            })
+    };
+    createManager();
+};
+init();
